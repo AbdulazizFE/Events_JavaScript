387d3bd45a9d6f3f7593de3e176b6d3d14fe84b3
Abdulaziz <91874905+AbdulazizFE@users.noreply.github.com>
2022-04-19 18:54:31 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,90 @@
+let btn = document.getElementById('btn');
+let inp = document.getElementById('inp'); 
+//onClick 
+
+//btn.onclick = () =>
+//{ document.body.style.background = 'red';}
+
+
+//addEventListener
+
+//btn.addEventListener('click', function(){
+//document.body.style.background = 'green';
+//btn.style.background = 'black';
+//btn.style.color = 'white';
+//})
+
+
+
+//onmouseup
+
+//btn.onmouseup = function()
+//{ document.body.style.background = 'green'; }
+
+
+//onmousedown
+
+//btn.onmousedown = function()
+//{ console.log('Hello World')  }
+
+
+//onmouseover
+
+//btn.onmouseover = function()
+//{ console.log('Hello World')  }
+
+
+//onmouseout
+
+//btn.onmouseout = function()
+//{ console.log('Hello World') }
+
+
+//onmousemove
+
+//let i = 1;
+//btn.onmousemove = function()
+//{ console.log('Hello World' + i++ )  }
+
+
+
+
+//onkeydown
+
+//let i = 1;
+//inp.onkeydown = function()
+//{ console.log('Hello World' + i++ )  }
+
+
+//onkeyup
+
+//let i = 1;
+//inp.onkeyup = function()
+//{ console.log('Hello World' + i++ )  }
+
+
+//onfocus
+
+//inp.onfocus = function()
+//{ inp.style.border = '2px solid red'; }
+
+//onblur
+
+//inp.onblur = function()
+//{ inp.style.border = 'none'; }
+
+
+
+
+//window, webside
+
+//onload
+
+//onload = function()
+//{ document.body.style.background = 'red'; }
+
+
+//onresize
+
+onresize = function()
+{ document.body.style.background = 'black'; }
